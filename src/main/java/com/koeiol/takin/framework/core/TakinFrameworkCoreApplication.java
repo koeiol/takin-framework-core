cafba359d9a7bd57570f592462f3dfa8a2d422ab
chiyb <chiyb@chiybdeMacBook-Pro.local>
2017-03-17 16:33:52 +0800

    Initial commit

---
+++ src/main/java/com/koeiol/takin/framework/core/TakinFrameworkCoreApplication.java
@@ -0,0 +1,12 @@
+package com.koeiol.takin.framework.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakinFrameworkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakinFrameworkCoreApplication.class, args);
+	}
+}
